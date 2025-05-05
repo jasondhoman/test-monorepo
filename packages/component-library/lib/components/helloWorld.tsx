ebb9162cda6bfdd7daba582fd4a8258d348d1657
jasondhoman <jasondhoman@gmail.com>
2025-05-05 07:44:00 -0500

    initial push

---
+++ packages/component-library/lib/components/helloWorld.tsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+  return (
+    <div>
+      <span>Hello from Comp Lib</span>
+    </div>
+  );
+}
+
+export { HelloWorld };
---
+++ packages/component-library/lib/main.ts
@@ -0,0 +1,3 @@
+import { HelloWorld } from './components/helloWorld.tsx';
+
+export { HelloWorld };
